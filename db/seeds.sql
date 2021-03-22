59417e006fca23ac0a5e13eb2411228dbf7bee57
Carly McVay <carlyjanemcvay@gmail.com>
2021-03-22 13:51:45 -0700

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Double', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Western Bacon', true);
+INSERT INTO burgers (burger_name) VALUES ('Avocado Burger');
